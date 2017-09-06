3630d13d65a9ea0a79aebdff46d6571862c734bd
Ravi Singhal <ravi.r.singhal@oracle.com>
2017-09-06 16:12:49 +0530

    use sequence

--- createSchema.sql
+++ createSchema.sql
@@ -1,8 +1,10 @@
 
 DROP TABLE Plate;
 
+create sequence plateid_sequence start with 1;
+
 CREATE TABLE Plate (
-    PlateID INTEGER primary key,
+    PlateID INTEGER primary key DEFAULT plateid_sequence.NEXTVAL,
     State CHAR(2),
     PlateNumber VARCHAR(10),
     Owner VARCHAR(100),
@@ -10,6 +12,6 @@ CREATE TABLE Plate (
     ImageURL VARCHAR(1000)
 );
 
-create sequence plateid_sequence start with 1;
+
 
 quit;
